38fabe48620f3951ea940133dbda6ec018ee6a04
Sanket Gujarathi <sanket.gujarathi@gmail.com>
2021-02-12 19:17:25 +0000

    Updated request mapping in controller

--- src/main/java/com/assignment/spring/controller/WeatherController.java
+++ src/main/java/com/assignment/spring/controller/WeatherController.java
@@ -3,10 +3,9 @@ package com.assignment.spring.controller;
 import com.assignment.spring.entity.WeatherEntity;
 import com.assignment.spring.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class WeatherController {
@@ -14,9 +13,9 @@ public class WeatherController {
     @Autowired
     private WeatherService weatherService;
 
-    @RequestMapping("/weather")
-    public WeatherEntity weather(HttpServletRequest request) {
-        String city = request.getParameter("city");
-        return weatherService.getWeather(city);
+    @GetMapping("/weather")
+    public WeatherEntity weather(@RequestParam String city) {
+        WeatherEntity weather = weatherService.getWeather(city);
+        return weather;
     }
 }
