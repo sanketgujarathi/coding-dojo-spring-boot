3d14f3b556126d93511390f462e8c8e4e9a63161
Sanket Gujarathi <sanket.gujarathi@gmail.com>
2021-02-12 19:36:17 +0000

    Created a separate response object to return from controller instead returning entity object

--- src/main/java/com/assignment/spring/controller/WeatherController.java
+++ src/main/java/com/assignment/spring/controller/WeatherController.java
@@ -1,8 +1,10 @@
 package com.assignment.spring.controller;
 
 import com.assignment.spring.entity.WeatherEntity;
+import com.assignment.spring.response.WeatherResponse;
 import com.assignment.spring.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,19 @@ public class WeatherController {
     private WeatherService weatherService;
 
     @GetMapping("/weather")
-    public WeatherEntity weather(@RequestParam String city) {
+    public ResponseEntity<WeatherResponse> weather(@RequestParam String city) {
         WeatherEntity weather = weatherService.getWeather(city);
-        return weather;
+        return ResponseEntity.ok(mapEntityToResponse(weather));
+    }
+
+    private WeatherResponse mapEntityToResponse(WeatherEntity weather){
+
+        WeatherResponse weatherResponse = new WeatherResponse();
+        weatherResponse.setId(weather.getId());
+        weatherResponse.setCity(weather.getCity());
+        weatherResponse.setCountry(weather.getCountry());
+        weatherResponse.setTemperature(weather.getTemperature());
+
+        return weatherResponse;
     }
 }
